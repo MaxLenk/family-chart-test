9df323299a3b770d810d6c863bbd977fa6ca8f38
Max Lenk <maxlenk@gmail.com>
2022-11-23 09:35:24 -0800

    Updates

--- precache-manifest.bf41b57ca9cf87f8e6c8d9329a0aea78.js
+++ precache-manifest.bf41b57ca9cf87f8e6c8d9329a0aea78.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "14d965c3b96a3def414cda0ce70be07d",
+    "revision": "7e6df02f2c497dd4599a36c661f91675",
     "url": "/family-chart-test/index.html"
   },
   {
-    "revision": "1749fa30af59dc411702",
+    "revision": "8372aaae95b715a2c27b",
     "url": "/family-chart-test/static/css/main.ad970e49.chunk.css"
   },
   {
-    "revision": "b56ea457c8f46ae136c9",
-    "url": "/family-chart-test/static/js/2.27cf6748.chunk.js"
+    "revision": "a8ba3c91cc0e93c7328f",
+    "url": "/family-chart-test/static/js/2.998e2290.chunk.js"
   },
   {
     "revision": "e5535f0aa14e2a42e7d2353d12a91d33",
-    "url": "/family-chart-test/static/js/2.27cf6748.chunk.js.LICENSE.txt"
+    "url": "/family-chart-test/static/js/2.998e2290.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1749fa30af59dc411702",
-    "url": "/family-chart-test/static/js/main.23c67db0.chunk.js"
+    "revision": "8372aaae95b715a2c27b",
+    "url": "/family-chart-test/static/js/main.6d11a05c.chunk.js"
   },
   {
     "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
-    "url": "/family-chart-test/static/js/main.23c67db0.chunk.js.LICENSE.txt"
+    "url": "/family-chart-test/static/js/main.6d11a05c.chunk.js.LICENSE.txt"
   },
   {
     "revision": "438fbe0dc8d7ea592a26",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/family-chart-test/precache-manifest.fa0f8d84fb41ef1859158d4c49bd89c1.js"
+  "/family-chart-test/precache-manifest.bf41b57ca9cf87f8e6c8d9329a0aea78.js"
 );
 
 self.addEventListener('message', (event) => {
